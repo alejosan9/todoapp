5c48a4b40cafa72c4ff3e7ff5b31b37657397ade
Alejandro <alejosan9@gmail.com>
2023-12-26 23:02:26 -0500

    Se continua con el tutorial

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,6 +1,8 @@
 import { Component, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { Task } from '../../models/task.model';
+
 @Component({
   selector: 'app-home',
   standalone: true,
@@ -9,18 +11,48 @@ import { CommonModule } from '@angular/common';
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
-  tasks = signal([
-    'Paso 1',
-    'Paso 2',
-    'Paso 3'
+  tasks = signal<Task[]>([
+    {
+      id: Date.now(),
+      title: 'Crear proyecto',
+      completed: false 
+    },
+    {
+      id: Date.now(),
+      title: 'Crear componente',
+      completed: false 
+    }
   ]);
 
   changeHandler(event: Event){
     const input = event.target as HTMLInputElement;
     const newTask = input.value;
-    this.tasks.update((tasks) => [...tasks, newTask]);
+    this.addTask(newTask);
     input.value = '';
   }
+
+  addTask(title: string){
+    const newTask = {
+      id: Date.now(),
+      title,
+      completed: false
+    };
+    this.tasks.update((tasks) => [...tasks, newTask]);
+  }
+
+  updateTask(index: number) {
+      this.tasks.update((tasks) => {
+        return tasks.map((task, position) => {
+          if (position === index) {
+            return {
+              ...task,
+              completed: !task.completed
+            }
+          }
+          return task;
+        })
+      })
+    }
 
   deleteTask(index: number){
     this.tasks.update((tasks) => tasks.filter((task, position) => position !== index))
